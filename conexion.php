dfc47b1027dbb5fd858b81b299df8006cf88c5db
AngelTah178 <pramdigorteriyaqui@gmail.com>
2026-03-15 08:14:50 -0500

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -5,7 +5,7 @@ $password = "";
 $dbname = "inventario";
 
 #creamos la conexion
-$conn = new mysqli($serverame, $username, $password, $dbname);
+$conn = new mysqli($servername, $username, $password, $dbname);
 #check the connection
 if ($conn->connect_error) {
     die("connection failed: " . $conn->connect_error);
