177a9774724dd4cd7ee4b120c814c55ee1e95fd5
AngelTah178 <pramdigorteriyaqui@gmail.com>
2026-03-17 10:13:01 -0500

    validar session

--- index.php
+++ index.php
@@ -1,9 +1,19 @@
+<?php
+session_start();
+include "conexion.php";
+if (!isset($_SESSION['logueado']) || $_SESSION['logueado'] !== true) {
+    header("Location: index.php");
+    exit;
+}
+?>
 <html>
-  <head>
+
+<head>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
-  </head>
-  <body>
+</head>
+
+<body>
     <!--Un botón sencillo que redirija a registrar user-->
     <!--Vamos por partes y actos, ACTO 1 - REGISTRAR EMPLEADO-->
     <button class="btn btn-primary" onclick="window.location.href='register.php'">Registrar usuario</button>
@@ -13,5 +23,6 @@
     <button class="btn btn-secondary" onclick="window.location.href='empleados.php'">Usuarios</button>
     <button class="btn btn-secondary" onclick="window.location.href='inventario.php'">Gestionar productos</button>
 
-  </body>
+</body>
+
 </html>
