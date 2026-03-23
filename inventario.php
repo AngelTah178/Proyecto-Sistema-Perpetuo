003243b44d04927eaa0046e1ee60596ac404729b
AngelTah178 <pramdigorteriyaqui@gmail.com>
2026-03-22 23:56:23 -0500

    Update inventario.php

--- inventario.php
+++ inventario.php
@@ -14,10 +14,10 @@ if ($rol != "admin" && $rol != "empleado") {
     exit();
 }
 
-$query = $conn->prepare("SELECT * FROM usuarios");
+$query = $conn->prepare("SELECT * FROM productos");
 $query->execute();
 $result = $query->get_result();
-$usuarios = $result->fetch_all(MYSQLI_ASSOC);
+$productos = $result->fetch_all(MYSQLI_ASSOC);
 ?>
 <html>
 
@@ -28,8 +28,68 @@ $usuarios = $result->fetch_all(MYSQLI_ASSOC);
 
 <body>
     <?php include 'include/navbar.php'; ?>
+    <!--Primera parte del CRUD, mostrar productos del inventario-->
+    <br><br><br><br><br>
+    <table class="table table-bordered">
+        <thead>
+            <tr>
+                <th>PRODUCTO_ID</th>
+                <th>CODIGO_BARRAS</th>
+                <th>SKU</th>
+                <th>NOMBRE</th>
+                <th>DESCRIPCION</th>
+                <th>PRECIO</th>
+                <th>FECHA_REGISTRO</th>
+                <th>LOTE_ID</th>
+                <th>MARCA_ID</th>
+                <th>CATEGORIA_ID</th>
+                <th>PROVEEDOR_ID</th>
+            </tr>
+        </thead>
 
-
+        <tbody>
+            <?php foreach ($productos as $p): ?>
+                <tr>
+                    <td>
+                        <?php echo $p['PRODUCTO_ID']; ?>
+                    </td>
+                    <td>
+                        <?php echo $p['CODIGO_BARRAS']; ?>
+                    </td>
+                    <td>
+                        <?php echo $p['SKU']; ?>
+                    </td>
+                    <td>
+                        <?php echo $p['NOMBRE']; ?>
+                    </td>
+                    <td>
+                        <?php echo $p['DESCRIPCION']; ?>
+                    </td>
+                    <td>
+                        <?php echo $p['PRECIO']; ?>
+                    </td>
+                    <td>
+                        <?php echo $p['FECHA_REGISTRO']; ?>
+                    </td>
+                    <td>
+                        <?php echo $p['LOTE_ID']; ?>
+                    </td>
+                    <td>
+                        <?php echo $p['MARCA_ID']; ?>
+                    </td>
+                    <td>
+                        <?php echo $p['CATEGORIA_ID']; ?>
+                    </td>
+                    <td>
+                        <?php echo $p['PROVEEDOR_ID']; ?>
+                    </td>
+                    <td>
+                        <button class="btn btn-danger" onclick="window.location.href='empleado.php'">Consultar</button>
+                    </td>
+                </tr>
+            <?php endforeach; ?>
+        </tbody>
+    </table>
 </body>
 
 </html>
