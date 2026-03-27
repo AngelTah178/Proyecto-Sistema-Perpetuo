dbba87aadc3c5108ff0b6880edd534f0487ac196
AngelTah178 <pramdigorteriyaqui@gmail.com>
2026-03-26 19:40:34 -0500

    e

--- index.php
+++ index.php
@@ -126,13 +126,79 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         <div class="d-flex justify-content-between align-items-center mb-3">
           <h4>Gestión de Usuarios</h4>
 
-          <button class="btn btn-custom" onclick="window.location.href='GestionarUsuarios.php'">
-            <i class="bi bi-person-plus"></i> Gestionar Usuarios
+          <button class="btn btn-custom" onclick="window.location.href='register.php'">
+            <i class="bi bi-person-plus"></i> Registrar usuario
           </button>
         </div>
       <?php endif; ?>
       </button>
+      <!-- FORMULARIO -->
 
+      <!-- BUSCADOR -->
+      <input type="text" id="buscador" class="form-control mb-3" placeholder="Buscar usuario...">
+
+      <!-- TABLA -->
+      <div class="table-responsive">
+        <table class="table table-hover align-middle">
+
+          <thead class="table-dark">
+            <tr>
+              <th>#</th>
+              <th>Nombre</th>
+              <th>Apellido</th>
+              <th>Correo</th>
+              <th>Rol</th>
+              <th>Estado</th>
+              <th class="text-center">Acciones</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            <?php $contador = 1; ?>
+            <?php foreach ($usuarios as $u): ?>
+              <tr>
+                <td>
+                  <?= $contador++; ?>
+                </td>
+                <td>
+                  <?= $u['NOMBRE']; ?>
+                </td>
+                <td>
+                  <?= $u['APELLIDO']; ?>
+                </td>
+                <td>
+                  <?= $u['CORREO']; ?>
+                </td>
+
+                <td>
+                  <span class="badge <?= $u['ROL'] == 'admin' ? 'bg-primary' : 'bg-secondary'; ?>">
+                    <?= $u['ROL']; ?>
+                  </span>
+                </td>
+
+                <td>
+                  <span class="badge <?= $u['ESTADO'] == 'activo' ? 'bg-success' : 'bg-danger'; ?>">
+                    <?= $u['ESTADO']; ?>
+                  </span>
+                </td>
+
+                <td class="text-center">
+
+                  <button class="btn btn-sm btn-warning"
+                    onclick="window.location.href='Admin/editarPerfil.php?id=<?= $u['ID_USUARIO']; ?>'">
+                    <i class="bi bi-pencil"></i>
+                  </button>
+
+                  <button class="btn btn-sm btn-danger"
+                    onclick="if(confirm('¿Eliminar usuario?')) { window.location.href='Admin/eliminarPerfil.php?PRODUCTO_ID=<?= $u['ID_USUARIO']; ?>'; }">
+                    <i class="bi bi-trash"></i>
+
+                </td>
+              </tr>
+            <?php endforeach; ?>
+          </tbody>
+        </table>
+      </div>
 
 
 
